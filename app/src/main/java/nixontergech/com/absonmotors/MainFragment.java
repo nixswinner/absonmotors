6b899326e65871913a631dcbda434a65cfb2613e
nixswinner <nixondeveloper@gmail.com>
2018-11-15 21:14:44 +0300

    initial commit

---
+++ app/src/main/java/nixontergech/com/absonmotors/MainFragment.java
@@ -0,0 +1,90 @@
+package nixontergech.com.absonmotors;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import ss.com.bannerslider.banners.Banner;
+import ss.com.bannerslider.banners.DrawableBanner;
+import ss.com.bannerslider.views.BannerSlider;
+
+
+public class MainFragment extends Fragment {
+
+
+    private OnFragmentInteractionListener mListener;
+
+    public MainFragment() {
+        // Required empty public constructor
+    }
+    public static MainFragment newInstance() {
+        MainFragment fragment = new MainFragment();
+        Bundle args = new Bundle();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view= inflater.inflate(R.layout.fragment_main,
+                container, false);
+
+        BannerSlider bannerSlider = view.findViewById(R.id.banner_slider1);
+        List<Banner> banners=new ArrayList<>();
+        //add banner using image url
+        //banners.add(new RemoteBanner("Put banner image url here ..."));
+        //add banner using resource drawable
+        /*banners.add(new DrawableBanner(R.drawable.greenafric));
+        banners.add(new DrawableBanner(R.drawable.greenafric1));
+        banners.add(new DrawableBanner(R.drawable.greenafric2));*/
+        bannerSlider.setBanners(banners);
+
+
+
+        return view;
+    }
+
+    // TODO: Rename method, update argument and hook method into UI event
+    public void onButtonPressed(Uri uri) {
+        if (mListener != null) {
+            mListener.onFragmentInteraction(uri);
+        }
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof OnFragmentInteractionListener) {
+            mListener = (OnFragmentInteractionListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        void onFragmentInteraction(Uri uri);
+    }
+}
